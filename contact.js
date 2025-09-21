81bb709eb7723a1ab0bdf71d3214e2e64de90c57
Sofiane.E <sofiane.elbaz1@icloud.com>
2025-09-21 14:57:15 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contact-form");
   const status = document.getElementById("form-status");
 
